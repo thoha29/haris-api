9252993352f52920deac788ee6fa7645d36a0bee
farandeffraim <93232915+farandeffraim@users.noreply.github.com>
2026-06-16 19:24:45 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,17 +4,53 @@ const { generateToken } = require('../helpers/jwt');
 
 exports.login = (req, res) => {
     const { username, password } = req.body;
-    db.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        if (results.length === 0) return res.status(404).json({ message: "username tidak terdaftar" });
 
-        const user = results[0];
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-            if (err) return res.status(500).json({ error: err.message });
-            if (!isMatch) return res.status(401).json({ message: "Password salah" });
+    db.query(
+        'SELECT * FROM users WHERE username = ?',
+        [username],
+        (err, results) => {
+            if (err) {
+                return res.status(500).json({
+                    error: err.message
+                });
+            }
 
-            const token = generateToken(user);
-            res.json({ token, role: user.role, id_user: user.id_user });
-        });
-    });
+            if (results.length === 0) {
+                return res.status(404).json({
+                    message: 'Username tidak terdaftar'
+                });
+            }
+
+            const user = results[0];
+
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+
+                console.log('====================');
+                console.log('Username DB:', user.username);
+                console.log('Password Input:', password);
+                console.log('Match:', isMatch);
+                console.log('====================');
+
+                if (err) {
+                    return res.status(500).json({
+                        error: err.message
+                    });
+                }
+
+                if (!isMatch) {
+                    return res.status(401).json({
+                        message: 'Password salah'
+                    });
+                }
+
+                const token = generateToken(user);
+
+                res.json({
+                    token,
+                    role: user.role,
+                    id_user: user.id_user
+                });
+            });
+        }
+    );
 };
